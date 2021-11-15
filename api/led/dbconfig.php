7305de4a4b21bf544b92bf1db67e077d640be407
Abishek Shrestha <56307374+abishekstha@users.noreply.github.com>
2021-11-15 21:30:16 +0545

    Add files via upload

---
+++ api/led/dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+
+
+define('DB_USER', "id17503723_admin"); 
+define('DB_PASSWORD', "S0ftw@ric@c0v3ntry");		
+define('DB_DATABASE', "id17503723_iot");
+define('DB_SERVER', "localhost");	
+?>
---
+++ api/weather/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_USER', "id17503723_admin"); 
+define('DB_PASSWORD', "S0ftw@ric@c0v3ntry");
+define('DB_DATABASE', "id17503723_iot"); 
+define('DB_SERVER', "localhost");			
+?>
